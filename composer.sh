76d479bf8b92f8c4b28109b0bece42a44f835fa8
abdul alim <shaikhalim4u@gmail.com>
2019-04-09 00:02:29 +0530

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -3,4 +3,4 @@
     npm install -g composer-rest-server@0.20.5
     npm install -g generator-hyperledger-composer@0.20.5
     npm install -g yo
-    git clone https://github.com/kMindz/fabric-dev-servers.git
+    https://github.com/aburaza/fabric-dev-servers.git
